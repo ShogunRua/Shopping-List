e732ab8277a8e1ccbca5f564466cd98a2e013ef6
Sergei Miroshkin <miroshkinsergei.3@gmail.com>
2021-08-06 00:43:44 +0300

    add ViewModel

---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.shoppinglist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.usecase.DeleteShopItemUseCase
+import com.example.shoppinglist.domain.usecase.EditShopItemUseCase
+import com.example.shoppinglist.domain.usecase.GetShopListUseCase
+
+class MainViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopListUseCase = DeleteShopItemUseCase(repository)
+    private val editShopListUseCase = EditShopItemUseCase(repository)
+
+    private val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList(){
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopList(shopItem: ShopItem){
+        deleteShopListUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopListUseCase.editShopItem(newItem)
+        getShopList()
+    }
+
+}
